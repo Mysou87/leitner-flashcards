af871394cb411a8b9cdf4a11012ab33789abcb57
Mysou87 <laureline.vanovermeir@gmail.com>
2026-06-07 14:39:44 +0200

    Update config.js
    Correction clé API

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://iuharjafrhwzhhggwzgy.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1aGFyamFmcmh3emhoZ2d3emhILCJyb2xlIjoiYW5vbiIsImlhdCI6MTc4MDgyMzMxOCwiZXhwIjoyMDk2Mzk5MzE4fQ.ieZT0hpGMcPQsJm6IjufzsZelZW3i25VSKhe_F8tqHk';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1aGFyamFmcmh3emhoZ2d3emd5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4MjMzMTgsImV4cCI6MjA5NjM5OTMxOH0.ieZT0hpGMcPQsJm6IjufzsZelZW3i25VSKhe_F8tqHk';
 
 // Mot de passe du panneau d'administration — à changer !
 const ADMIN_PASSWORD = 'leitner2025';
